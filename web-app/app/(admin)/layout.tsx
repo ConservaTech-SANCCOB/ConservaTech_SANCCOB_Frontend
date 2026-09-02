42d3d27e437b49df6ae6da5b8e6833411dc4ecba
Lofentse Moagi: ST10438345 <164502650+St10438345@users.noreply.github.com>
2026-09-03 00:40:03 +0200

    Dashboard Page
    - Added layout logic for the dashboard
    - Included mock data just to see if the layout is correct

---
+++ web-app/app/(admin)/layout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "@/components/layout/sidebar";
+import Topbar from "@/components/layout/topbar";
+
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen bg-slate-50">
+      <Sidebar/>
+      <div className="flex-1 flex flex-col min-w-0">
+        <Topbar />
+        <main className="flex-1 p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
